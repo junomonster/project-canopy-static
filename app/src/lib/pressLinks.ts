de4aba235a1f66021123e945e3e70508c62d04e6
Daejun Kim <reset@theori.io>
2026-06-18 13:28:12 +0900

    feat: update PRESS_URL to include UTM parameters for tracking

--- app/src/lib/pressLinks.ts
+++ app/src/lib/pressLinks.ts
@@ -1,1 +1,2 @@
-export const PRESS_URL = 'https://project-plasma.org/notice/10';
+export const PRESS_URL =
+  'https://project-plasma.org/notice/10?utm_source=canopy&utm_medium=homepage&utm_campaign=launched';
