dff060eee793544b1d093f09d8c02695bd0478da
Juno Im <juno@theori.io>
2026-07-13 22:11:34 +0900

    a

--- app/src/components/Header.tsx
+++ app/src/components/Header.tsx
@@ -17,6 +17,9 @@ export default function Header() {
           <NavLink to="/faq" className="nav-link" end>
             {t('nav.faq')}
           </NavLink>
+          {/*<NavLink to="/blog" className="nav-link">
+            {t('nav.blog')}
+          </NavLink>*/}
           <NavLink to="/join" className="nav-link nav-cta" end>
             {t('nav.join')}
           </NavLink>
